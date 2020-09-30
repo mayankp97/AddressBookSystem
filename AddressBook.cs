e23122af13363eda860f0c9e64d115789a368764
Emma Paris <mayankpurohit2012@gmail.com>
2020-09-30 14:25:10 +0530

    [Mayank] Add Functionality to edit contact

---
+++ AddressBook.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AddressBookSystem
+{
+    class AddressBook
+    {
+        public Dictionary<string, Contact> contacts { get; set; }
+        public int numberOfContacts { get; set; }
+
+        public AddressBook()
+        {
+            this.contacts = new Dictionary<string, Contact>();
+        }
+
+        public void AddContact(Contact contact)
+        {
+            contacts.Add(contact.firstName, contact);
+            numberOfContacts = contacts.Count;
+        }
+        public void EditContact(string firstName, Contact contact)
+        {
+            if (!contacts.ContainsKey(firstName))
+            {
+                Console.WriteLine("Input Contact Name does not exist.");
+                return;
+            }
+            if (firstName == contact.firstName)
+                contacts[firstName] = contact;
+            else
+            {
+                contacts.Remove(firstName);
+                AddContact(contact);
+            }
+
+            Console.WriteLine("Contact Edited Successfully!");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -3,61 +3,75 @@ using System.Collections.Generic;
 
 namespace AddressBookSystem
 {
-    class AddressBook
-    {
-        public List<Contact> contacts { get; set; }
-        public int numberOfContacts { get; set; }
-
-        public AddressBook()
-        {
-            this.contacts = new List<Contact>();
-        }
-
-        public void AddContact(Contact contact)
-        {
-            contacts.Add(contact);
-            numberOfContacts = contacts.Count;
-        }
-    }
     class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Address Book System!");
 
-            var contact = TakeInputForContact();
-            var addressbook1 = new AddressBook();
-            addressbook1.AddContact(contact);
+            //Adding a contact to contact book
+            var addressbook = new AddressBook();
+            addressbook.AddContact(new Contact("A", "Raja", 
+                new Address("Bazar A", "Kolkata", "WB", 1452), "12345", "gmail.com"));
+
+            //Editing by using first name of the contact
+            Console.Write("Enter the name of the person to edit contact details :");
+            var firstName = Console.ReadLine();
+
+            if (!addressbook.contacts.ContainsKey(firstName))
+                Console.WriteLine("No record(s) found");
+            else
+            {
+                var contactToEdit = addressbook.contacts[firstName];
+                Console.WriteLine("################ The existing details are ############");
+                DisplayContact(contactToEdit);
+                Console.WriteLine("################ Enter the new details ############");
+                var contact = TakeInputForContact();
+                addressbook.EditContact(firstName, contact);
+            }
+            
         }
 
         static Contact TakeInputForContact()
         {
-            Console.WriteLine("Enter First Name : ");
+            Console.Write("Enter First Name : ");
             var firstName = Console.ReadLine();
 
-            Console.WriteLine("Enter Last Name : ");
+            Console.Write("Enter Last Name : ");
             var lastName = Console.ReadLine();
 
-            Console.WriteLine("Enter Address Line One : ");
+            Console.Write("Enter Address Line One : ");
             var addressLineOne = Console.ReadLine();
 
-            Console.WriteLine("Enter City : ");
+            Console.Write("Enter City : ");
             var city = Console.ReadLine();
 
-            Console.WriteLine("Enter State : ");
+            Console.Write("Enter State : ");
             var state = Console.ReadLine();
 
-            Console.WriteLine("Enter Zip : ");
+            Console.Write("Enter Zip : ");
             var zip = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("Enter Phone No. : ");
+            Console.Write("Enter Phone No. : ");
             var phoneNumber = Console.ReadLine();
 
-            Console.WriteLine("Enter Email : ");
+            Console.Write("Enter Email : ");
             var email = Console.ReadLine();
 
             var address = new Address(addressLineOne, city, state, zip);
             return new Contact(firstName, lastName, address, phoneNumber, email);
+        }
+
+        static void DisplayContact(Contact contact)
+        {
+            Console.WriteLine(" First Name : {0}",contact.firstName);
+            Console.WriteLine(" Last Name : {0}",contact.lastName);
+            Console.WriteLine(" Address Line One : {0}", contact.address.addressLineOne);
+            Console.WriteLine(" City : {0}",contact.address.city);
+            Console.WriteLine(" State : {0}",contact.address.state);
+            Console.WriteLine(" Zip : {0}",contact.address.zip);
+            Console.WriteLine(" Phone No. : {0}",contact.phoneNumber);
+            Console.WriteLine(" Email : {0}",contact.email);
         }
 
 
